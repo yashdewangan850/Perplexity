90b307cf85247fec313808628a5a7ab4bddd66a7
yashdewangan850-hub <yashdewangan850@gmail.com>
2026-08-04 23:09:06 +0530

    add node millre

--- Backend/src/middleware/auth.middleware.js
+++ Backend/src/middleware/auth.middleware.js
@@ -1,10 +1,6 @@
 import jwt from "jsonwebtoken";
 
-
-
-
 export function authUser(req, res, next) {
-
     const token = req.cookies.token;
 
     if (!token) {
@@ -12,23 +8,28 @@ export function authUser(req, res, next) {
             message: "Unauthorized",
             success: false,
             err: "No token provided"
-        })
+        });
     }
 
     try {
-
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        // Debug Logs
+        console.log("========== AUTH MIDDLEWARE ==========");
+        console.log("Token:", token);
+        console.log("Decoded Token:", decoded);
+        console.log("====================================");
 
         req.user = decoded;
 
         next();
-
     } catch (err) {
+        console.error("JWT Error:", err.message);
+
         return res.status(401).json({
             message: "Unauthorized",
             success: false,
             err: "Invalid token"
-        })
+        });
     }
-
 }
