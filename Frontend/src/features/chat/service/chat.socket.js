68f005cd7eafa6a7e84785c26aefc8b19e4b736e
yashdewangan850-hub <yashdewangan850@gmail.com>
2026-08-04 13:32:28 +0530

    fix spcket io

--- Frontend/src/features/chat/service/chat.socket.js
+++ Frontend/src/features/chat/service/chat.socket.js
@@ -1,14 +1,22 @@
 import { io } from "socket.io-client";
 
+let socket;
 
 export const initializeSocketConnection = () => {
+    if (socket) return socket;
 
-    const socket = io("http://localhost:3000", {
+    socket = io("https://perplexity-1-5xj4.onrender.com", {
         withCredentials: true,
-    })
+        transports: ["websocket", "polling"],
+    });
 
     socket.on("connect", () => {
-        console.log("Connected to Socket.IO server")
-    })
+        console.log("✅ Connected to Socket.IO server");
+    });
 
-}
+    socket.on("connect_error", (err) => {
+        console.error("❌ Socket Error:", err.message);
+    });
+
+    return socket;
+};
