e20fabb28ab72028be75913f6d636158eeb1af80
yashdewangan850-hub <yashdewangan850@gmail.com>
2026-08-11 20:51:54 +0530

    remove import veryfy

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { register, verifyEmail, login, getMe } from "../controllers/auth.controller.js";
+import { register, login, getMe } from "../controllers/auth.controller.js";
 import { registerValidator, loginValidator } from "../validators/auth.validator.js";
 import { authUser } from "../middleware/auth.middleware.js";
 
